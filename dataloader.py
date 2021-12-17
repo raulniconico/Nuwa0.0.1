7f8d90e8ed2629754f8c67a0ba1792ca2e17563a
raulniconico <raulniconico@outlook.com>
2021-12-17 15:00:53 +0100

    add comments

--- dataloader.py
+++ dataloader.py
@@ -5,11 +5,13 @@ import random
 class Dataset:
     def __init__(self,X,y,proportion=0.8,shuffle=True, mini_batch=0):
         """
-        :param X: ndarray
-                    feature data
-        :param y: labels
-        :param proportion: number between 0 and 1
-        :param shuffle: boolean, whether apply data shuffle
+        Dataset class provide tools to manage dataset
+
+        :param X: ndarray, features, highly recommand ndarray
+        :param y: ndarray, labels
+        :param proportion: number between 0 and 1, the proportion of train dataset and test dataset
+        :param shuffle: boolean,
+        :param mini_batch mini batch size, 0 by default, in this case no mini batch size dataset will be generated
         """
         self.X = X
         self.y = y
@@ -33,6 +35,7 @@ class Dataset:
     def distribute(self):
         """
         This function will automatically distribute train and test dataset
+        call this function to reshuffle all the dataset and also generate new train and test set
         """
         n = np.shape(self.X)[0]
         samples = np.concatenate((self.X,self.y),axis=1)
@@ -63,9 +66,12 @@ class Dataset:
         return Dataset(self.testset[:, 0:self.X.shape[1]], self.testset[:, self.X.shape[1]:])
 
     def getminiset(self):
+        """
+        get mini sets with mini batch size
+        :return: Dataset array
+        """
         spilit_list = np.arange(self.mini_batch, self.allset.shape[0], self.mini_batch)
         minisets = np.split(self.allset, spilit_list)
         for i in range(len(minisets)):
             self.minisets.append(Dataset(minisets[i][:, 0:self.X.shape[1]], minisets[i][:, self.X.shape[1]:],shuffle =False, mini_batch=self.mini_batch))
         return self.minisets
-
--- network.py
+++ network.py
@@ -4,15 +4,30 @@ import numpy as np
 
 class NN:
     import numpy as np
-    def __init__(self,dataset):
+    def __init__(self, dataset):
         """
-        :param dataset:
+        This class contains Activation function util class, Layer class for construct networks, it contains several extend classes like LinearLayer, Conv2D etc.
+
+        Examples:
+
+        layer_list = [NN.Layer('Linear',3,10,'sigmoid',BN=True), NN.Layer('Linear',10,100,'sigmoid',BN=True),
+              NN.Layer('Linear',100,10,'sigmoid',BN=True),NN.Layer('Linear',10,3,'none') ]
+
+        dataset = Dataset(X, y, mini_batch= 64)
+
+        nn = NN(dataset)
+
+        layer_list is a list has 4 layers all are Layer class. Note that here we don't use LinearLayer,
+        to use LinearLayer, replace NN.Layer('Linear',3,10,'sigmoid',BN=True) as NN.LinearLayer(,3,10,'sigmoid',BN=True) or
+        NN.LinearLayer(3,10,'sigmoid'), NN.BN()
+
+        :param dataset: Dataset class
         """
         # self.input = input
         self.dataset = dataset
         self.layer_list = []
 
-    def addlayers(self,layers):
+    def addlayers(self, layers):
         self.layer_list = layers
 
     def getlayers(self):
@@ -20,6 +35,11 @@ class NN:
 
     # activation functions
     class ActivationFunc:
+        """
+        ActivationFunc is an util class with different types of activation function.
+        it can
+        """
+
         @staticmethod
         def sigmoid(x):
             """
@@ -29,11 +49,15 @@ class NN:
 
         @staticmethod
         def ReLU(x):
+            """
+            :param x: ndarray,
+            :return:
+            """
             return np.maximum(0, x)
 
         @staticmethod
         def LeakyReLU(x):
-            return  np.where(x > 0, x, x * 0.01)
+            return np.where(x > 0, x, x * 0.01)
 
         @staticmethod
         def tanh(x):
@@ -45,12 +69,21 @@ class NN:
 
     # Layer class
     class Layer:
-        def __init__(self, type, input_dim, output_dim, activation, BN = False):
+        def __init__(self, type, input_dim, output_dim, activation, BN=False):
             """
+            Define a layer contains activation function or other normalization.
+
             :param type: Layer type, choose 'Linear', 'Conv' etc
             :param input_dim: input dim or previous layer's output
             :param output_dim: output dim of this layer
-            :param activation: activation function, 'none' for no activation
+            :param activation: activation function, it now support "sigmoid", "ReLU", "LeakyReLU", "tanh" and "none" for no activation function
+            :param BN, batch normalization , Default False
+
+            Examples:
+
+            A linear layer with input dim = 3 and output dim = 10, following batch normalization and a sigmoid activation function
+            NN.Layer('Linear',3,10,'sigmoid',BN=True)
+
             """
             self.type = type
             self.input_dim = input_dim
@@ -68,7 +101,7 @@ class NN:
             return self.type
 
         def getact(self, x):
-            func_name = "NN.ActivationFunc."+self.activation
+            func_name = "NN.ActivationFunc." + self.activation
             func = eval(func_name)
             return func(x)
 
@@ -79,16 +112,33 @@ class NN:
             return self.BN
 
     class LinearLayer(Layer):
+        """
+        Define a linear layer
+
+        As same as Layer except no need to clarify type
+        """
+
         def __init__(self, input_dim, output_dim):
             self.type = "Linear"
             self.input_dim = input_dim
             self.output_dim = output_dim
 
-
     class Conv2DLayer(Layer):
-        def __init__(self, image_size, kernel_size, stride, padding):
+        """
+        Define a 2D convolutional layer_
+        """
+
+        def __init__(self, input_size, kernel_size, stride, padding):
+            """
+            initialize 2D conv layer
+
+            :param input_size: Union[tuple, ndarray]  layer's input size
+            :param kernel_size:  Union[tuple, ndarray] layer's kernel size
+            :param stride: Int
+            :param padding: Int
+            """
             self.type = "Conv2D"
-            self.image_size = image_size
+            self.input_size = input_size
             self.kernel_size = kernel_size
             self.stride = stride
             self.padding = padding
@@ -107,5 +157,8 @@ class NN:
 
     class BN(Layer):
         def __init__(self):
+            """
+            Define a batch normalization layer
+            """
             self.type = "BN"
-            self.activation ="none"
+            self.activation = "none"
